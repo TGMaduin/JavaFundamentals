ca1e1cd7d2fd955a435a654856ff678622027f0c
TGMaduin <tgmaduin@gmail.com>
2026-06-09 14:09:55 +0200

    Complete exercise 1

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -4,14 +4,15 @@ package se.lexicon;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        String name = "Christoffer";
+        int age = 41;
+        String city = "Väckelsång";
+        IO.println("====================");
+        IO.println("     My Profile     ");
+        IO.println("====================");
+        IO.println("Name : " + name);
+        IO.println("Age  : " + age);
+        IO.println("City : " + city);
+        IO.println("====================");
     }
 }
